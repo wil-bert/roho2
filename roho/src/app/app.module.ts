47711c802209f0d4586f334491dd0a2831efbefd
wil-bert <ianwilbuts@gmail.com>
2023-09-19 19:41:38 +0300

    tool bar

--- roho/src/app/app.module.ts
+++ roho/src/app/app.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatButtonModule} from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
+import { MatIconModule} from '@angular/material/icon';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { MatListModule} from '@angular/material/list';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatTableModule} from '@angular/material/table';
+import { MatBadgeModule} from '@angular/material/badge';
+import { MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatMenuModule} from '@angular/material/menu';
+
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -14,7 +30,20 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatListModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatBadgeModule,
+    MatSnackBarModule,
+    MatMenuModule,
+    MatPseudoCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- roho/src/app/home/home.component.ts
+++ roho/src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+ 
 })
 export class HomeComponent {
-
+  events: string[] = [];
+  open!: boolean;
 }
+
+
+
+
+
+
+
