6a425fa6b95dce854111ab98e35f8715972cd086
wil-bert <ianwilbuts@gmail.com>
2023-09-25 13:45:44 +0300

    add router links

--- roho/src/app/app-routing.module.ts
+++ roho/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ContactComponent } from './contact/contact.component';
+import { DonateComponent } from './donate/donate.component';
 
 const routes: Routes = [{path : 'home', component: HomeComponent},
-{path: '', redirectTo: 'home', pathMatch: 'full'}];
+{path : 'activities', component: ActivitiesComponent},
+{path : 'contact', component: ContactComponent},
+{path : 'donate', component: DonateComponent},
+{path: '', redirectTo: ' home ', pathMatch: 'full'}];
+
+
 
 
 @NgModule({
